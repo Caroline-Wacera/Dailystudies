8bd9e7165f42b7c25741516822bb06c36a571176
Caroline-Wacera <carolinewacera4@gmail.com>
2025-01-27 18:17:36 +0300

    comparison in strings and variables

---
+++ comparingstrings&variables.py
@@ -0,0 +1,11 @@
+number_1 = 10
+number_2 = 20
+print(number_1 >= number_2  )
+print(number_1 <= number_2)
+print(number_1 == number_2)
+my_name = "Caroline"
+your_name = "Peter"
+random_name = "Peter"
+print(my_name == your_name)
+print(my_name != your_name)
+print(your_name != random_name)
