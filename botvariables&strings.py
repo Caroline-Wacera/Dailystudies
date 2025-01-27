aa8f8e1db1af2234cfc018dd788cdf763f9ea663
Caroline-Wacera <carolinewacera4@gmail.com>
2025-01-27 17:28:06 +0300

    mixing variables and strings

---
+++ botvariables&strings.py
@@ -0,0 +1,8 @@
+print("What is your name?")
+name = "Caroline"
+print(f"You have a lovely name, {name}")
+age = 29
+bot_age = 3
+age_difference = age - bot_age
+print(f"How old are you {name}?")
+print(f"I am {age_difference} older than you.")
